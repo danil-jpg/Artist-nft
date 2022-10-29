6bf3eac213ddde77530b670d2242b5d62c80bbfb
WeeRbe <danil.aliev97@gmail.com>
2022-10-29 22:39:55 +0300

    lending page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,13 @@
+import Swiper from 'swiper';
+
 import burger from './modules/burger.js'
+import lending from './modules/lending.js'
 
 window.addEventListener('DOMContentLoaded', () => {
-  const lendingPage = document.querySelector('#burgerPage')
-
+  const lendingPage = document.querySelector('#lendingMain')
   if (lendingPage) {
     burger()
+    lending()
   }
 
 })
