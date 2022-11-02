461a0ee61f9c72c214bde5640e590cd2233c55c2
Danil <danil.polovinkin.1994@gmail.com>
2022-11-02 13:27:59 +0300

    completed basic logic on admin page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,19 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper'
 
 import burger from './modules/burger.js'
 import lending from './modules/lending.js'
+import admin from './modules/admin.js'
 
 window.addEventListener('DOMContentLoaded', () => {
   const lendingPage = document.querySelector('#lendingMain')
+  const adminPage = document.querySelector('.adminPage')
+
   if (lendingPage) {
     burger()
     lending()
   }
 
+  if (adminPage) {
+    admin()
+  }
 })
