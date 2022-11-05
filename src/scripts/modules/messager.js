b04f41afd719d398d568622124110cc52dbe6e94
Danil <danil.polovinkin.1994@gmail.com>
2022-11-05 12:35:02 +0300

    added and adaptated another one block

--- src/scripts/modules/messager.js
+++ src/scripts/modules/messager.js
@@ -1,11 +1,16 @@
+import Swiper from 'swiper'
+
 function messager () {
-  console.log(1)
   const menuTrigger = document.querySelector('.module-header__burger-wr')
   const menu = document.querySelector('.module__left-wr')
 
   menuTrigger.addEventListener('click', () => {
     menu.classList.toggle('active')
   })
-}
 
+  const swiperBlock = new Swiper('.swiper', {
+    spaceBetween: 28,
+    slidesPerView: 4.6
+  })
+}
 export default messager
