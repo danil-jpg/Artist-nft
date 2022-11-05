df8e008dcd65d17543954b897c4e4e2ea5a0cce9
WeeRbe <danil.aliev97@gmail.com>
2022-11-05 02:12:43 +0200

    page auction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,17 +2,22 @@ import Swiper from 'swiper'
 
 import burger from './modules/burger.js'
 import lending from './modules/lending.js'
+import auction from './modules/auction.js'
 import admin from './modules/admin.js'
 
 window.addEventListener('DOMContentLoaded', () => {
   const lendingPage = document.querySelector('#lendingMain')
   const adminPage = document.querySelector('.adminPage')
-
+  const auctionPage = document.querySelector('#auctionPage')
   if (lendingPage) {
     burger()
     lending()
   }
-
+  if (auctionPage){
+    burger()
+    lending()
+    auction()
+  }
   if (adminPage) {
     admin()
   }
