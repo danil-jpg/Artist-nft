0ad9bca56a14b11711dd5d443d9809e1c05d9fe2
WeeRbe <danil.aliev97@gmail.com>
2022-11-07 20:29:12 +0200

    page payment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,16 @@ import burger from './modules/burger.js'
 import lending from './modules/lending.js'
 import auction from './modules/auction.js'
 import pricing from './modules/pricing.js'
+import payment from './modules/payment.js'
 import admin from './modules/admin.js'
 
 window.addEventListener('DOMContentLoaded', () => {
   const lendingPage = document.querySelector('#lendingMain')
   const adminPage = document.querySelector('.adminPage')
   const auctionPage = document.querySelector('#auctionPage')
-  const pricingPage = document.querySelector('#PricingPage')
+  const pricingPage = document.querySelector('#pricingPage')
+  const paymentPage = document.querySelector('#paymentPage')
+
   if (lendingPage) {
     burger()
     lending()
@@ -22,6 +25,10 @@ window.addEventListener('DOMContentLoaded', () => {
   if(pricingPage){
     burger()
     pricing()
+  }
+  if (paymentPage){
+    burger()
+    payment()
   }
   if (adminPage) {
     admin()
